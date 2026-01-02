5fbb36d7b5270797baad985349d98bf753210a79
Haroon Rasheed <56812559+kharoojn274@users.noreply.github.com>
2026-01-02 22:48:55 +0300

    Update prisma.ts

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,7 +1,13 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-// Ensure singleton pattern to avoid multiple instances in dev (Next.js/Hot reload)
+declare global {
+  // allow prisma to be a global var in dev
+  // so hot reload doesn't create multiple instances
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
 const prisma = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
